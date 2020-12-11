56e2f182a17203cc41806c05dafd13f8e5265f04
Aleksey Belchenkov <u608110@gmail.com>
2020-12-11 21:00:56 +0300

    Header Links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// Components
+import Header from "./components/Header";
+
+// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import ProductDetails from "./pages/ProductDetails";
 
 export default function App() {
   return (
       <Router>
+        <Header />
         <Switch>
             <Route path="/" exact>
                 <Home />
@@ -17,11 +25,23 @@ export default function App() {
             <Route path="/about">
                 <About />
             </Route>
+            <Route path="/login">
+                <Login />
+            </Route>
             <Route path="/cart">
                 <Cart />
             </Route>
-            <Route path="/products">
+            <Route exact path="/products">
                 <Products />
+            </Route>
+            <Route
+                path="/products/:id"
+                children={
+                    <ProductDetails />
+                }
+            />
+            <Route path="/checkout">
+                <Checkout />
             </Route>
             <Route path="*">
                 <Error />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../assets/logo.svg"
 
 export default function Header() {
-  return <h1>hello from header</h1>;
+  return (
+      <header className="header">
+        <img
+            src={logo}
+            alt="Vintage Tech Logo"
+            className="logo"
+        />
+        <nav>
+          <ul>
+            <div>
+              <li>
+                <Link to="/">
+                  <i className="fas fa-home mr-3" />
+                  Home
+                </Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/about">
+                  <i className="fas fa-question-circle mr-3" />
+                  About
+                </Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/products">
+                  <i className="fas fa-gifts mr-3" />
+                  Products
+                </Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/login">
+                  <i className="fas fa-sign-in-alt mr-3" />
+                  Login
+                </Link>
+              </li>
+            </div>
+            <div>
+              <li>
+                <Link to="/cart">
+                  <i className="fas fa-shopping-cart mr-3" />
+                  Cart
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </header>
+  );
 }
